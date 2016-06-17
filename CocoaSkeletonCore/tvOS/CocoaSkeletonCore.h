34a67f376c0646e0e3f978e717382af01866dff1
Adam Kornafeld <adam.kornafeld@mediafriendsinc.com>
2016-06-17 14:53:14 -0400

    Add tvOS target

---
+++ CocoaSkeletonCore/tvOS/CocoaSkeletonCore.h
@@ -0,0 +1,19 @@
+//
+//  CocoaSkeletonCore-tvOS.h
+//  CocoaSkeletonCore-tvOS
+//
+//  Created by Adam Kornafeld on 6/17/16.
+//  Copyright © 2016 Adam Kornafeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaSkeletonCore-tvOS.
+FOUNDATION_EXPORT double CocoaSkeletonCore_tvOSVersionNumber;
+
+//! Project version string for CocoaSkeletonCore-tvOS.
+FOUNDATION_EXPORT const unsigned char CocoaSkeletonCore_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaSkeletonCore_tvOS/PublicHeader.h>
+
+
