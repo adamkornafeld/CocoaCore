66794fa086ef4b4d174c3a8caacf2b76b00cf842
Adam Kornafeld <adam.kornafeld@mediafriendsinc.com>
2016-06-17 14:50:34 -0400

    Add watchOS target

---
+++ CocoaSkeletonCore/watchOS/CocoaSkeletonCore-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  CocoaSkeletonCore-watchOS.h
+//  CocoaSkeletonCore-watchOS
+//
+//  Created by Adam Kornafeld on 6/17/16.
+//  Copyright © 2016 Adam Kornafeld. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for CocoaSkeletonCore-watchOS.
+FOUNDATION_EXPORT double CocoaSkeletonCore_watchOSVersionNumber;
+
+//! Project version string for CocoaSkeletonCore-watchOS.
+FOUNDATION_EXPORT const unsigned char CocoaSkeletonCore_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaSkeletonCore_watchOS/PublicHeader.h>
+
+
